6092206b87c88d0c89da4693e4f6ff0c42e57bfb
milesuno <milesoluku@gmail.com>
2020-07-23 10:19:38 +0100

    Geolocation converted to react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Geolocation from "./components/geolocation/geolocation";
+
+function App() {
+	return (
+		<div className="App">
+			<Geolocation />
+		</div>
+	);
+}
+
+export default App;
